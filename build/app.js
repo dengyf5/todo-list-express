d8ee6da411d4d320b312a630cfb3e9ce66420120
everbrez <everbrez@gmail.com>
2018-06-02 17:48:27 +0800

    add database

--- build/app.js
+++ build/app.js
@@ -1,6 +1,34 @@
-import http from 'http'
+'use strict';
 
-http.createServer((req, res) => {
-    res.end('HelloWorld!')
-}).listen(8888);
-console.log('listening to 8888');
+var _http = require('http');
+
+var _http2 = _interopRequireDefault(_http);
+
+var _mongoose = require('mongoose');
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var db = _mongoose2.default.connect('mongodb://localhost/myItems');
+var Schema = _mongoose2.default.Schema;
+var Module = _mongoose2.default.model;
+
+var PersonSchema = new Schema({
+    name: String
+});
+
+var PersonModule = Module('Person', PersonSchema);
+
+var person1 = new PersonModule({
+    name: "everbrez"
+});
+
+console.log(person1.name);
+
+// http.createServer((req, res) => {
+//     res.end('HelloWorld!')
+// }).listen(8888);
+// console.log('listening to 8888');
+
+//# sourceMappingURL=app.js.map
