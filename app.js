a50c95d868b07457e5ac927e2658f231b9b5b9d0
everbrez <everbrez@gmail.com>
2018-06-05 17:14:01 +0800

    app.js demo code change

--- app.js
+++ app.js
@@ -1,49 +1,7 @@
-import http from 'http'
-import mongoose from 'mongoose'
-const db = mongoose.createConnection('mongodb://localhost/myItems');
-const Schema = mongoose.Schema;
-
-const PersonSchema = new Schema({
-    name: String
-});
-
-const PersonModule = mongoose.model('person', PersonSchema);
-
-const person = new PersonModule({
-    name: "kizana"
-});
-
-// 
-let collection = PersonModule.collection.name
-console.log(db.dropCollection(collection));
-// PersonModule.findById('5b125f6e0ce934122c34662f', (err, docs) => {
-//     if(err){
-//         console.log(`error`)
-//     } else {
-//         console.log(docs)
-//     }
-// })
+import http from "http";
 
 
-
-// PersonModule.deleteMany({name: 'everbrez'},(err) => {
-//     if(!err){
-//         console.log(`success!`);
-//     } else {
-//         console.log(`error!`);
-//     }
-// });
-// person.save(err => {
-//     if(err) {
-//         console.log(`error`);
-//     } else {
-//         console.log(`success save ${person.name}`);
-//     }
-// });
-
-
-
-// http.createServer((req, res) => {
-//     res.end('HelloWorld!')
-// }).listen(8888);
-// console.log('listening to 8888');
+http.createServer((req, res) => {
+    res.end('HelloWorld!')
+}).listen(8888);
+console.log('listening to 8888');
