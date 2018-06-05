3881978dc27cbba65a8a7c1a40355124bfedc39e
everbrez <everbrez@gmail.com>
2018-06-05 19:09:55 +0800

    add Todo model

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-import http from "http";
+import mongoose from "mongoose";
+import Todo from "./models/todos/todos";
+
+let data = {
+    item: 'Go shopping',
+    tag: 'life',
+    description: 'buy some tissue and some food!', 
+    userId: 233,
+    level: 2,
+    finishedTime: new Date('2018-06-06'),
+    createTime: new Date(),
+};
 
 
-http.createServer((req, res) => {
-    res.end('HelloWorld!')
-}).listen(8888);
-console.log('listening to 8888');
+mongoose.connect('mongodb://localhost/todos');
+
+Todo(data).save(err => {
+    if(err) {
+        console.log(`error!`);
+    } else {
+        console.log(`success!`)
+    }
+});
---
+++ models/todos/todos.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+    item: String,
+    tag: String,
+    description: String, 
+    userId: Number,
+    level: Number,
+    finishedTime: Date,
+    createTime: Date,
+});
+
+
+const Todo = mongoose.model('todos',todoSchema);
+
+export default Todo;
